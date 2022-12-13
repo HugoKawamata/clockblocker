543bed1aa640a2c1b3b2c074bbad7ea36fc00329
Hugo Kawamata <hugokawamata@gmail.com>
2022-12-13 12:18:25 +1000

    ✨ Add pie chart to centre of clock

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { DateTime } from "luxon";
+
+const SECONDS_IN_DAY = 86400
+
+const secondsPastMidnight = DateTime.now().toSeconds() - DateTime.now().startOf("day").toSeconds()
+const dayPercentComplete = (secondsPastMidnight / SECONDS_IN_DAY) * 100
 
 function App() {
+  document.documentElement.style.setProperty('--progress', dayPercentComplete);
+
   return (
     <div id="main">
       <div className="clock">
-        <div class="outer-clock-face">
-          <div class="marking zero"></div>
-          <div class="marking one"></div>
-          <div class="marking two"></div>
-          <div class="marking three"></div>
-          <div class="marking four"></div>
-          <div class="marking five"></div>
-          <div class="inner-clock-face">
+        <div className="outer-clock-face">
+          <div className="marking zero"></div>
+          <div className="marking one"></div>
+          <div className="marking two"></div>
+          <div className="marking three"></div>
+          <div className="marking four"></div>
+          <div className="marking five"></div>
+          <div className="inner-clock-face">
+            <div
+              className="clock-block"
+              // style="--p:60;--b:10px;--c:purple;" 
+            >
+            </div>
           </div>
         </div>
       </div>
