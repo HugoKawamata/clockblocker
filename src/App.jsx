98be5508349f39901b28ec1c3a62886bc2541259
Hugo Kawamata <hugokawamata@gmail.com>
2022-12-14 18:14:39 +1000

    ✨ Make it update on an interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react"
 import "./App.css"
 import { DateTime } from "luxon"
 import Clock from "./components/Clock"
 
-// TODO: Set this up on an interval to update every 10 sec or so
-const secondsPastMidnight = DateTime.now().toSeconds() - DateTime.now().startOf("day").toSeconds()
 
 function App() {
+  const [time, setTime] = useState(DateTime.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(DateTime.now()), 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const secondsPastMidnight = time.toSeconds() - time.startOf("day").toSeconds()
+
   return (
     <div id="main">
       <Clock amOrPm="am" secondsPastMidnight={secondsPastMidnight} />
